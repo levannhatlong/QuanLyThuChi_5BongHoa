e6e966a8f25d4420272408fdf23da30b870f10d5
ADMIN-PC\Admin <nminhhoang769@gmail.com>
2025-12-23 22:19:07 +0700

    tao trang danh sach giao dich, lien ket ngan hang, xuat du lieu, chi tiet thong bao, update doi mat khau

---
+++ QUANLYTHUCHI_5BongHoa/app/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.quanlythuchi_5bonghoa"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.quanlythuchi_5bonghoa"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    
+    packaging {
+        resources {
+            excludes += listOf(
+                "META-INF/NOTICE.md",
+                "META-INF/LICENSE.md",
+                "META-INF/NOTICE.txt",
+                "META-INF/LICENSE.txt"
+            )
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    
+    // JTDS driver để kết nối SQL Server
+    implementation("net.sourceforge.jtds:jtds:1.3.1")
+    
+    // JavaMail để gửi email OTP
+    implementation("com.sun.mail:android-mail:1.6.7")
+    implementation("com.sun.mail:android-activation:1.6.7")
+}
